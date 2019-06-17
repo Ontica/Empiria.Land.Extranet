64aa9673e0120aafd8962be527df0bf5a6b78d71
jmcota <jmcota@ontica.org>
2019-06-16 20:22:38 -0400

    Add index.html, styles, logos, icons and other assets

---
+++ src/app/core/index.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+export { CoreService } from './core.service';
+
+export { ExceptionHandler } from './general/exception-handler';
+export { Exception } from './general/exception';
+
+export { Assertion } from './general/assertion';
+export { Validate } from './general/validate';
+
+export { SessionService } from './general/session.service';
+export { HttpService } from './http/http.service';
+export { LoggerService } from './general/logger.service';
+
+export { AuthenticationService } from './security/authentication.service';
+export { SecurityGuardService } from './security/security-guard.service';
