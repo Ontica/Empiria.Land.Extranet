e7c50f9388a3777df5c9b416e4c50a61b2745013
jmcota <jmcota@ontica.org>
2019-09-10 18:37:08 -0500

    Update user interface according to notaries' users requirements

--- src/app/user-interface/electronic-filing/request-list/request-list-item.component.ts
+++ src/app/user-interface/electronic-filing/request-list/request-list-item.component.ts
@@ -5,7 +5,7 @@
  * See LICENSE.txt in the project root for complete license information.
  */
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { EFilingRequest, PreventiveNote } from '@app/domain/models';
 
@@ -14,8 +14,36 @@ import { EFilingRequest, PreventiveNote } from '@app/domain/models';
   selector: 'emp-one-request-list-item',
   templateUrl: './request-list-item.component.html',
 })
-export class RequestListItemComponent {
+export class RequestListItemComponent implements OnChanges {
 
   @Input() request: EFilingRequest;
+
+  preventiveNoteDueDate = '';
+
+  ngOnChanges() {
+    this.calculateDueDate();
+  }
+
+  private calculateDueDate() {
+    if (!this.request || !this.request.transaction || !this.request.transaction.presentationDate) {
+      this.preventiveNoteDueDate = '';
+      return;
+    }
+
+    const oneDay = 24 * 60 * 60 * 1000;
+
+    const presentation = new Date(this.request.transaction.presentationDate).getTime();
+    const now = new Date().getTime();
+    const dueDate = (presentation + (30 * oneDay)) - now;
+
+    if (dueDate < 0) {
+      this.preventiveNoteDueDate = `Vencido`;
+      return;
+    }
+
+    const diffDays = Math.round(dueDate / oneDay);
+
+    this.preventiveNoteDueDate = `Vigencia: ${diffDays} días`;
+  }
 
 }
